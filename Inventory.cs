d32d1a5f19a01767eb44459ab64bf58e544d7dbb
gharjare <gharjarechetan@gmail.com>
2022-11-06 20:07:02 +0530

    Added File 2

---
+++ Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagment
+{
+    public class Inventory
+    {
+        public TypesofRice typesofRice;
+    }
+
+    public class TypesofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
