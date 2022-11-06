625850487fce4581e7ed9ee3bcaf0bbde1ddfffb
gharjare <gharjarechetan@gmail.com>
2022-11-06 20:05:43 +0530

    Added File 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InventoryManagment
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Achal\source\repos\InventoryManagment\InventoryManagment\jsconfig1.json";
+
+            FetchData fetchData = new FetchData();
+            Inventory data = fetchData.Read(path);
+            Console.WriteLine(data.typesofRice.name);
+            Console.WriteLine(data.typesofRice.weight);
+            Console.WriteLine(data.typesofRice.price);
+        }
+    }
+}
